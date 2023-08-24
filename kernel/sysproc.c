02f21f4eaee8c341db7e01e6817c9455bb45a2c6
lr580 <2277379149@qq.com>
2023-08-24 23:07:43 +0800

    完成lab3

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -75,12 +75,53 @@ sys_sleep(void)
   return 0;
 }
 
+int pgaccess(pagetable_t pagetable,uint64 start_va, int page_num, uint64 result_va)
+{
+  if (page_num > 64)
+  {
+    panic("pgaccess: too much pages");
+    return -1;
+  }
+  unsigned int bitmask = 0;
+  int cur_bitmask = 1;
+  int count = 0;
+  uint64 va = start_va;
+  pte_t *pte;
+  for (; count < page_num; count++, va += PGSIZE)
+  {
+    if ((pte = walk(pagetable, va, 0)) == 0)
+      panic("pgaccess: pte should exist");
+    if ((*pte & PTE_A))
+    {
+      bitmask |= (cur_bitmask<<count);
+      *pte &= ~PTE_A;
+    }
+  }
+  copyout(pagetable,result_va,(char*)&bitmask,sizeof(bitmask));
+  return 0;
+}
 
 #ifdef LAB_PGTBL
 int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 va;
+  if(argaddr(0,&va)<0){//0th param is va
+    return -1;
+  }
+  int num;
+  if(argint(1,&num)<0){//1st param is num
+    return -1;
+  }
+  uint64 res;
+  if(argaddr(2,&res)<0){
+    return -1;
+  }
+  struct proc* p = myproc();
+  if(pgaccess(p->pagetable, va, num, res) < 0) {
+    return -1;
+  }
   return 0;
 }
 #endif
